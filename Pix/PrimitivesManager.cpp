313863d8b4264cfb66669488fc3d38160456c921
ArthurH-cmd <arthurheymans101@gmail.com>
2026-02-05 19:36:12 -0800

    Plz God Im begging you

---
+++ Pix/PrimitivesManager.cpp
@@ -0,0 +1,84 @@
+#include "PrimitivesManager.h"
+#include "Rasterizer.h"
+#include "Clipper.h"
+
+PrimitivesManager* PrimitivesManager::Get()
+{
+	static PrimitivesManager sInstance;
+	return &sInstance;
+}
+
+PrimitivesManager::PrimitivesManager()
+{
+}
+
+// Start accepting vertices
+bool PrimitivesManager::BeginDraw(Topology topology)
+{
+	mVertexBuffer.clear();
+	mTopology = topology;
+	mDrawBegin = true;
+	return true;
+}
+// Add vertices to the list, onyl if drawing is enable
+void PrimitivesManager::AddVertex(const Vertex& vertex)
+{
+	if (mDrawBegin)
+	{
+		mVertexBuffer.push_back(vertex);
+	}
+}
+// Send all the stored vertices to the rasterizer as specified topology
+bool PrimitivesManager::EndDraw()
+{
+	if (!mDrawBegin)
+	{
+		return false;
+	}
+
+	Rasterizer* rasterizer = Rasterizer::Get();
+
+	switch (mTopology)
+	{
+	case Topology::Point:
+	{
+		for (uint32_t i = 0; i < mVertexBuffer.size(); ++i)
+		{
+			if (!Clipper::Get()->ClipPoint(mVertexBuffer[i]))
+			{
+				rasterizer->DrawPoint(mVertexBuffer[i]);
+			}
+		}
+	}
+	break;
+	case Topology::Line:
+	{
+		for (uint32_t i = 1; i < mVertexBuffer.size(); i += 2)
+		{
+			if (!Clipper::Get()->ClipLine(mVertexBuffer[i - 1], mVertexBuffer[i]))
+			{
+				rasterizer->DrawLine(mVertexBuffer[i - 1], mVertexBuffer[i]);
+			}
+		}
+	}
+	break;
+	case Topology::Triangle:
+	{
+		for (uint32_t i = 2; i < mVertexBuffer.size(); i += 3)
+		{
+			std::vector<Vertex> triangle = { mVertexBuffer[i - 2], mVertexBuffer[i - 1], mVertexBuffer[i] };
+			if (!Clipper::Get()->ClipTriangle(triangle))
+			{
+				for (uint32_t v = 2; v < triangle.size(); ++v)
+				{
+					rasterizer->DrawTriangle(triangle[0], triangle[v - 1], triangle[v]);
+				}
+			}
+		}
+	}
+	break;
+	default:
+		return false;
+	}
+}
+
---
+++ Pix/Rasterizer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <XEngine.h>
+#include "Vertex.h"
+
+enum class FillMode
+{
+	WireFrame,
+	Solid
+};
+
+class Rasterizer
+{
+public:
+	static Rasterizer* Get();
+
+public:
+	void SetColor(X::Color color);
+
+	void SetFillMode(FillMode fillMode);
+
+	void DrawPoint(int x, int y);
+
+	void DrawPoint(const Vertex& vertex);
+	void DrawLine(const Vertex& a, const Vertex& b);
+	void DrawTriangle(const Vertex& a, const Vertex& b, Vertex& c);
+
+private:
+	void DrawFilledTriangle(const Vertex& a, const Vertex& b, const Vertex& c);
+	X::Color mColor = X::Colors::White;
+	FillMode mFillMode = FillMode::Solid;
+};
